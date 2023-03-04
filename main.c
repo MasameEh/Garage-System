3f39346e9ec40f4fdddd1b3119011bd4042b2521
mohamed sameh <92492113+MasameEh@users.noreply.github.com>
2023-03-05 01:38:26 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -20,8 +20,8 @@
 
 int main(void)
 {
-	dio_set_pin_direction(IR1_PORT,IR1_PIN,DIO_DIRECTION_INPUT);
-	dio_set_pin_direction(IR2_PORT,IR2_PIN,DIO_DIRECTION_INPUT);
+	dio_set_pin_direction(IR1_PORT, IR1_PIN, DIO_DIRECTION_INPUT);
+	dio_set_pin_direction(IR2_PORT, IR2_PIN, DIO_DIRECTION_INPUT);
 	LCD_init();
 	LCD_move_curser(4,0);
 	LCD_send_string("0 Cars");
@@ -31,7 +31,8 @@ int main(void)
 	
     while (1) 
     {
-		if (dio_get_pin(IR2_PORT,IR2_PIN) ==  DIO_LEVEL_HIGH && counter < 3 && flag1 == 0)
+		//enter
+		if (dio_get_pin(IR2_PORT, IR2_PIN) ==  DIO_LEVEL_HIGH && counter < 3 && flag1 == 0)
 		{
 			counter++;
 			LCD_move_curser(4,0);
@@ -48,28 +49,29 @@ int main(void)
 			timer1_fastPWM_A(.999);
  			_delay_ms(500);
 			timer1_fastPWM_A(1.499999);
- 		}else if (dio_get_pin(IR2_PORT,IR2_PIN) ==  DIO_LEVEL_LOW)
+ 		}
+		 else if (dio_get_pin(IR2_PORT, IR2_PIN) ==  DIO_LEVEL_LOW)
  		{
 			 flag1 = 0;
  		}
-		 
-		 if (dio_get_pin(IR1_PORT,IR1_PIN) ==  DIO_LEVEL_HIGH && counter > 0 && flag2 == 0)
+		 //exit
+		 if (dio_get_pin(IR1_PORT, IR1_PIN) ==  DIO_LEVEL_HIGH && counter > 0 && flag2 == 0)
 		 {
 			 counter--;
-			 LCD_clearscreen();
-			 LCD_move_curser(4,0);
-			 LCD_send_char(counter+48);
-			 LCD_move_curser(6,0);
-			 LCD_send_string("Cars");
-			 LCD_move_curser(0,1);
-			 LCD_send_string("Garage has space");
-			
+			LCD_clearscreen();
+			LCD_move_curser(4,0);
+			LCD_send_char(counter+48);
+			LCD_move_curser(6,0);
+			LCD_send_string("Cars");
+			LCD_move_curser(0,1);
+			LCD_send_string("Garage has space");
 			flag2 = 1;
+			
 			timer1_fastPWM_B(.999);
 			_delay_ms(500);
 			timer1_fastPWM_B(1.499999);
-			
-		 }else if (dio_get_pin(IR1_PORT,IR1_PIN) ==  DIO_LEVEL_LOW)
+		 }
+		 else if (dio_get_pin(IR1_PORT, IR1_PIN) ==  DIO_LEVEL_LOW)
 		 {
 			 flag2 = 0;
 		 }
