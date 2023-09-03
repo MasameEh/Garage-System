316e4319bd1062b9bbbd22cfda9ea1e4ad3f129c
mohamed sameh <92492113+MasameEh@users.noreply.github.com>
2023-09-03 06:22:01 +0300

    Add garage.h

---
+++ garage.h
@@ -0,0 +1,36 @@
+/*
+ * garage.h
+ *
+ * Created: 9/3/2023 5:54:59 AM
+ *  Author: Mohamed Sameh
+ */ 
+
+
+#ifndef GARAGE_H_
+#define GARAGE_H_
+
+/* Section : Includes */
+#include "timer.h"
+#include "LCD.h"
+
+/* Section : Macro Declarations */
+#define IR1_PORT			DIO_PORTC
+#define IR1_PIN				DIO_PIN0
+
+#define IR2_PORT			DIO_PORTC
+#define IR2_PIN				DIO_PIN1
+
+/* Section : Macro Functions Declarations */
+
+
+/* Section : Data Types Declarations  */
+
+
+/* Section : Functions Declarations */
+
+
+
+
+
+
+#endif /* GARAGE_H_ */
